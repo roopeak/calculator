d6b6ac8193a761db28c34b31a6d5dbd551b9f64b
roopeak <roopeak@protonmail.com>
2022-05-24 09:20:16 +0300

    Operate-function created

--- scripts.js
+++ scripts.js
@@ -1,3 +1,5 @@
+const operator = [add, subtract, multiply, divide];
+
 function add(a, b) {
     console.log(a + b);
 }
@@ -17,7 +19,13 @@ function divide(a, b) {
     console.log(a / b);
 }
 
+
+function operate(operator, a, b) {
+    
+}
+
 add(5,5);
 subtract(5,5);
 multiply(5,5);
 divide(5,5);
+operate();
