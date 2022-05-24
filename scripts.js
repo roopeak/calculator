73ff3f87dd0188b099e6316d1e2daa4aaf4681e6
roopeak <roopeak@protonmail.com>
2022-05-24 12:50:12 +0300

    Working buttons

--- scripts.js
+++ scripts.js
@@ -1,29 +1,80 @@
-const operator = [add, subtract, multiply, divide];
+let screenDisplay = document.querySelector('.screenDisplay');
+let numberA = '';
+let numberB = '';
+
+const buttons = document.querySelectorAll('button[id]');
+const operators = document.querySelectorAll('.operators');
+const clearBtn = document.getElementById('clear');
+const equalBtn = document.getElementById('equal');
+
+
+let number = buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        inputNumber(this.textContent)
+    });
+});
+
+
+const inputNumber = number => {
+    const screenNumber = screenDisplay.innerHTML;
+    if (screenNumber.length < 16)
+        screenDisplay.innerHTML = parseInt(screenNumber + number).toString();
+}
+
+
+let operator = buttons.forEach(btn => {
+    btn.addEventListener('click', function() {
+        inputOperator(this.textContent)
+    });
+});
+
+
+const inputOperator = operator => {
+    const screenOperator = screenDisplay.innerHTML;
+    screenDisplay.innerHTML = operator;
+
+}
+
+
 
 function add(a, b) {
-    console.log(a + b);
+    // return a + b;
 }
 
 
 function subtract(a, b) {
-    console.log(a - b);
+    // return a - b;
 }
 
 
 function multiply(a, b) {
-    console.log(a * b);
+    // return a * b;
 }
 
 
 function divide(a, b) {
-    console.log(a / b);
+    // return a / b;
 }
 
 
-function operate(operator, a, b) {
+function operate(operator) {
+    let a = screenDisplay.textContent;
+    let b = screenDisplay.textContent;
+
+    if(operator == '+')
+        prompt('Hello')
+    else if(operator == '-')
+        subtract(a,b)
+    else if(operator == '*')
+        multiply(a, b)
+    else
+        divide(a, b)
+}
+
+
+function appendNumber() {
     
 }
-
 add(5,5);
 subtract(5,5);
 multiply(5,5);
