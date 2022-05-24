cbb80045dad471afbb18c7e63680d267c58a3aff
roopeak <roopeak@protonmail.com>
2022-05-24 09:15:25 +0300

    Created functions

---
+++ scripts.js
@@ -0,0 +1,23 @@
+function add(a, b) {
+    console.log(a + b);
+}
+
+
+function subtract(a, b) {
+    console.log(a - b);
+}
+
+
+function multiply(a, b) {
+    console.log(a * b);
+}
+
+
+function divide(a, b) {
+    console.log(a / b);
+}
+
+add(5,5);
+subtract(5,5);
+multiply(5,5);
+divide(5,5);
