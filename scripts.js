eb33e965e2604bfafe77a05197c3a6d8469cf88a
roopeak <roopeak@protonmail.com>
2022-05-25 10:35:35 +0300

    Started over

--- scripts.js
+++ scripts.js
@@ -1,78 +1,45 @@
-let screenDisplay = document.querySelector('.screenDisplay');
-let numberA = '';
-let numberB = '';
-
-const buttons = document.querySelectorAll('button[id]');
-const operators = document.querySelectorAll('.operators');
-const clearBtn = document.getElementById('clear');
-const equalBtn = document.getElementById('equal');
-
-
-
-let number = buttons.forEach(btn => {
-    btn.addEventListener('click', function() {
-        inputNumber(this.textContent)
-    });
-});
-
-
-const inputNumber = number => {
-    const screenNumber = screenDisplay.innerHTML;
-    if (screenNumber.length < 16)
-        screenDisplay.innerHTML = parseInt(screenNumber + number).toString();
-}
-
-
-let operator = buttons.forEach(btn => {
-    btn.addEventListener('click', function() {
-        inputOperator(this.textContent)
-    });
-});
-
-
-const inputOperator = operator => {
-    const screenOperator = screenDisplay.innerHTML;
-    screenDisplay.innerHTML = operator;
-}
-
-
-
 function add(a, b) {
-    // return a + b;
+    console.log(a + b);
 }
 
 
 function subtract(a, b) {
-    // return a - b;
+    console.log(a - b);
 }
 
 
 function multiply(a, b) {
-    // return a * b;
+    console.log(a * b);
 }
 
 
 function divide(a, b) {
-    // return a / b;
+    console.log(a / b);
 }
 
 
-function operate(operator) {
-    let a = '';
-    let b = '';
+function operate(operator, a, b) {
+    a = numberA;
+    b = numberB;
 
-    if(operator == '+')
-        prompt('Hello')
-    else if(operator == '-')
-        subtract(a,b)
-    else if(operator == '*')
-        multiply(a, b)
-    else
-        divide(a, b)
+    switch(operator) {
+        case '+':
+            return add(a, b);
+        case '-':
+            return subtract(a, b);
+        case '*':
+            return multiply(a, b);
+        case '/':
+            if (b === 0) return null;
+            else return divide(a, b);
+        default:
+            return null;
+    }      
 }
 
+operate('*', 5, 5);
 
-add(5,5);
-subtract(5,5);
-multiply(5,5);
-divide(5,5);
+// add(5,5);
+// subtract(5,5);
+// multiply(5,5);
+// divide(5,5);
