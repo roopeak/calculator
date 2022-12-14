05d6368110e7452422640c4ebbf7b38a63ecfa46
roopeak <roopeak@protonmail.com>
2022-12-14 22:16:07 +0200

    Declared buttons

---
+++ script.js
@@ -0,0 +1,22 @@
+// Buttons
+const clearBtn = document.querySelector(".clear");
+const delBtn = document.querySelector(".delete");
+
+const oneBtn = document.querySelector(".one");
+const twoBtn = document.querySelector(".two");
+const threeBtn = document.querySelector(".three");
+const fourBtn = document.querySelector(".four");
+const fiveBtn = document.querySelector(".five");
+const sixBtn = document.querySelector(".six");
+const sevenBtn = document.querySelector(".seven");
+const eightBtn = document.querySelector(".eight");
+const nineBtn = document.querySelector(".nine");
+
+const sumBtn = document.querySelector(".sum");
+const subtractBtn = document.querySelector(".subtract");
+const divideBtn = document.querySelector(".divide");
+const multiplyBtn = document.querySelector(".multiply");
+
+// Display
+const prevNumbers = document.querySelector(".display-prev-numbers");
+const currNumbers = document.querySelector(".display-curr-numbers");
