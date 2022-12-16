5b33642c4f202718b862cac49bffb94cc77d3151
roopeak <roopeak@protonmail.com>
2022-12-16 09:51:40 +0200

    Added calculate function

--- script.js
+++ script.js
@@ -2,7 +2,14 @@
 const clearBtn = document.querySelector(".clear");
 const delBtn = document.querySelector(".delete");
 
-let numberBtn = document.querySelectorAll(".number");
+const numberBtn = document.querySelectorAll(".number");
+const operatorBtn = document.querySelectorAll(".operator");
+const equalsBtn = document.querySelector(".equals");
+
+let firstOperand = "";
+let secondOperand = "";
+let calcOperator = "";
+
 
 // const oneBtn = document.querySelector(".one");
 // const twoBtn = document.querySelector(".two");
@@ -14,19 +21,63 @@ let numberBtn = document.querySelectorAll(".number");
 // const eightBtn = document.querySelector(".eight");
 // const nineBtn = document.querySelector(".nine");
 
-const sumBtn = document.querySelector(".sum");
-const subtractBtn = document.querySelector(".subtract");
-const divideBtn = document.querySelector(".divide");
-const multiplyBtn = document.querySelector(".multiply");
+// const sumBtn = document.querySelector(".sum");
+// const subtractBtn = document.querySelector(".subtract");
+// const divideBtn = document.querySelector(".divide");
+// const multiplyBtn = document.querySelector(".multiply");
 
 // Display
 const prevNumbers = document.querySelector(".display-prev-numbers");
 const currNumbers = document.querySelector(".display-curr-numbers");
 
-
+// Populates display with numbers
 for (let i = 0; i < numberBtn.length; i++) {
     let number = numberBtn[i].textContent
     numberBtn[i].addEventListener("click", function() {
         currNumbers.innerHTML += number
     })
+}
+
+for (let i = 0; i < operatorBtn.length; i++) {
+    let operator = operatorBtn[i].textContent
+    operatorBtn[i].addEventListener("click", function() {
+        firstOperand = parseFloat(currNumbers.innerHTML);
+        calcOperator = operator;
+        currNumbers.innerHTML += operator;
+        prevNumbers.innerHTML += currNumbers.innerHTML;
+        currNumbers.innerHTML = "";
+    })
+}
+
+equalsBtn.addEventListener("click", function() {
+    secondOperand = parseFloat(currNumbers.innerHTML);
+    prevNumbers.innerHTML += currNumbers.innerHTML
+    currNumbers.innerHTML = "";
+    calculate()
+});
+
+function calculate() {
+    switch (calcOperator) {
+        case "+": currNumbers.innerHTML += firstOperand + secondOperand
+        break;
+
+        case "-": currNumbers.innerHTML += firstOperand - secondOperand
+        break;
+
+        case "*": currNumbers.innerHTML += firstOperand * secondOperand
+        break;
+
+        case "/": currNumbers.innerHTML += firstOperand / secondOperand
+        break;
+    }
+
+    equalsBtn.disabled = true;
+}
+
+clearBtn.addEventListener("click", clearDisplay);
+
+function clearDisplay() {
+    currNumbers.innerHTML = "";
+    prevNumbers.innerHTML = "";
+    equalsBtn.disabled = false;
 }
