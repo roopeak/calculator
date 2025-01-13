d9adc7418e70eb2097a1d43fe038802e00991ac9
Roope Kärkkäinen <roopeak@protonmail.com>
2025-01-13 09:51:59 +0200

    Calculations with two numbers

--- scripts.js
+++ scripts.js
@@ -1,3 +1,8 @@
+let clearCounter = 0;
+let a = '';
+let b = '';
+let currentOperator = '';
+
 // Display
 const currentNumber = document.querySelector('.current-number');
 const previousNumber = document.querySelector('.previous-number');
@@ -5,35 +10,32 @@ const previousNumber = document.querySelector('.previous-number');
 // Buttons
 const numberButtons = document.querySelectorAll('.num-btn');
 const operationButtons = document.querySelectorAll('.operation-btn');
+const eraseButton = document.querySelector('.erase-btn');
+const clearButton = document.querySelector('.clear-btn');
+const equalButton = document.querySelector('.equal-btn');
 
 numberButtons.forEach((button) => {
-  button.addEventListener('click', () => {
-		currentNumber.textContent += button.textContent;
-	})
+  button.addEventListener('click', () => addNumber(button.textContent))
 });
 
-console.log(operationButtons)
-
 operationButtons.forEach((button) => {
+	button.addEventListener('click', () => setOperation(button.textContent))
+});
 
-	button.addEventListener('click', () => {
-		console.log(button.textContent);
-		switch (button.textContent) {
-			case '+':
-				add(a, b);
-			case '-':
-				subtract(a, b);
-			case '*':
-				multiply(a, b);
-			case '÷':
-				divide(a, b);
-			case 'clear':
-				clearValues(1);
-			case 'erase':
-				eraseValue();
-		}
-	})
-})
+eraseButton.addEventListener('click', () => eraseValue());
+clearButton.addEventListener('click', () => clearValues());
+equalButton.addEventListener('click', () => evaluate());
+
+function addNumber(number) {
+	currentNumber.textContent += number;
+}
+
+function setOperation(operator) {
+	a = currentNumber.textContent;
+	currentOperator = operator;
+	previousNumber.textContent = `${a} ${currentOperator}`;
+	currentNumber.textContent = '';
+}
 
 function add(a, b) {
 	return a + b;
@@ -51,10 +53,29 @@ function divide(a, b) {
 	return a / b;
 }
 
-function clearValues(timesCleared) {
-	let clearCounter = 0;
+function operate(operator, a, b) {
+	a = Number(a);
+	b = Number(b);
 
-	if (clearCounter === 0) {
+  switch (operator) {
+    case '+':
+      return add(a, b)
+    case '-':
+      return subtract(a, b)
+    case '*':
+      return multiply(a, b)
+    case '÷':
+      if (b === 0) return null
+      else return divide(a, b)
+    default:
+      return null
+  }
+}
+
+function clearValues() {
+	if (currentNumber.textContent === '' && previousNumber.textContent != '') {
+		previousNumber.textContent = '';
+	} else if (clearCounter === 0) {
 		currentNumber.textContent = '';
 		clearCounter++;
 	} else if (clearCounter === 1) {
@@ -69,4 +90,15 @@ function eraseValue() {
 	if (stringToErase.length > 0) {
 		currentNumber.textContent = stringToErase.substring(0, stringToErase.length - 1);
 	}
+}
+
+function evaluate() {
+	if (currentNumber.textContent === '') return;
+	b = currentNumber.textContent;
+	previousNumber.textContent = operate(currentOperator, a, b);
+	currentNumber.textContent = '';
+	// console.log('This is a: ', a, 'This is b: ', b)
+	// console.log(operate(currentOperator, a, b));
+	// console.log(`${a} ${currentOperator} ${b}`);
+
 }
